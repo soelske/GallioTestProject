a5145aea19d51036033b3a432537546558a732c4
Bart Suelze <bartsuelze@hotmail.com>
2018-12-25 22:01:18 +0100

    direct path of test dll in gallio batch file

--- TestProgram.Test/TextFile1.bat
+++ TestProgram.Test/TextFile1.bat
@@ -1,3 +1,3 @@
 SET GALLIO_RUNTIME_PATH=C:\Users\PC\source\repos\mbunit-v3\src\Gallio\Gallio\bin
-"C:\Users\PC\source\repos\mbunit-v3\src\Extensions\Echo\Gallio.Echo\bin\Gallio.Echo.exe" TestProgram.Test.dll /r:AutoCAD /rp:AcadAttachToExisting=false /rp:AcadExePath="C:\Program Files\Autodesk\AutoCAD 2017\acad.exe"
+"C:\Users\PC\source\repos\mbunit-v3\src\Extensions\Echo\Gallio.Echo\bin\Gallio.Echo.exe" "C:\TeamCity\buildAgent\work\a3354fc635062806\TestProgram.Test\bin\x64\Debug\TestProgram.Test.dll" /r:AutoCAD /rp:AcadAttachToExisting=false /rp:AcadExePath="C:\Program Files\Autodesk\AutoCAD 2017\acad.exe"
 pause
