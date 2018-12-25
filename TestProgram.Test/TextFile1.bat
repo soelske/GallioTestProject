2637455e880ad5af63e0d409ff87ca74a573b785
Bart Suelze <bartsuelze@hotmail.com>
2018-12-25 20:29:55 +0100

    added acad exec path to gallio test batch

--- TestProgram.Test/TextFile1.bat
+++ TestProgram.Test/TextFile1.bat
@@ -1,4 +1,4 @@
 ﻿pause
 SET GALLIO_RUNTIME_PATH=C:\Users\PC\source\repos\mbunit-v3\src\Gallio\Gallio\bin
-"C:\Users\PC\source\repos\mbunit-v3\src\Extensions\Echo\Gallio.Echo\bin\Gallio.Echo.exe" TestProgram.Test.dll /r:AutoCAD
+"C:\Users\PC\source\repos\mbunit-v3\src\Extensions\Echo\Gallio.Echo\bin\Gallio.Echo.exe" TestProgram.Test.dll /r:AutoCAD /rp:AcadAttachToExisting=false /rp:AcadExePath="C:\Program Files\Autodesk\AutoCAD 2017\acad.exe"
 pause
