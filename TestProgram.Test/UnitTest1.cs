e6b1486b112572160127c595def3f277dda5bad4
Bart Suelze <bartsuelze@hotmail.com>
2019-01-02 20:40:12 +0100

    failing test

--- TestProgram.Test/UnitTest1.cs
+++ TestProgram.Test/UnitTest1.cs
@@ -19,37 +19,50 @@ namespace TestProgram.Test
             var area = Utilities.GetCircleArea(id);
             Assert.AreEqual(area, Math.PI * (0.9 * 0.9));
         }
-    }
 
-    public static class ExtensionMethods
-    {
-        public static ObjectId Create<T>(this Database database, Action<T> action)
-            where T : Entity, new()
+        [Test]
+        public void TestMethod2()
         {
-            using (var tr = database.TransactionManager.StartTransaction())
-            {
-                try
-                {
-                    // Open model space
-                    var blockTable = (BlockTable)tr
-                        .GetObject(database.BlockTableId, OpenMode.ForRead);
-                    var modelSpace = (BlockTableRecord)tr
-                        .GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForWrite);
+            var doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            var db = doc.Database;
 
-                    var obj = new T();
-                    obj.SetDatabaseDefaults();
-                    action(obj);
-                    var objectId = modelSpace.AppendEntity(obj);
-                    tr.AddNewlyCreatedDBObject(obj, true);
-                    tr.Commit();
-                    return objectId;
-                }
-                catch (Exception)
-                {
-                    tr.Abort();
-                    throw;
-                }
+            var id = db.Create<Circle>(circle => circle.Radius = 0.9);
+
+            var area = Utilities.GetCircleArea(id);
+            Assert.AreEqual(area, Math.PI * (0.9 * 0.91));
+        }
+    }
+}
+
+public static class ExtensionMethods
+{
+    public static ObjectId Create<T>(this Database database, Action<T> action)
+        where T : Entity, new()
+    {
+        using (var tr = database.TransactionManager.StartTransaction())
+        {
+            try
+            {
+                // Open model space
+                var blockTable = (BlockTable)tr
+                    .GetObject(database.BlockTableId, OpenMode.ForRead);
+                var modelSpace = (BlockTableRecord)tr
+                    .GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForWrite);
+
+                var obj = new T();
+                obj.SetDatabaseDefaults();
+                action(obj);
+                var objectId = modelSpace.AppendEntity(obj);
+                tr.AddNewlyCreatedDBObject(obj, true);
+                tr.Commit();
+                return objectId;
+            }
+            catch (Exception)
+            {
+                tr.Abort();
+                throw;
             }
         }
     }
 }
+
