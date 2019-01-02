ee4d09771bd9a1b766714fa4fefa29c966742072
Bart Suelze <bartsuelze@hotmail.com>
2019-01-02 13:34:54 +0100

    test

--- TestProgram.Test/TextFile1.bat
+++ TestProgram.Test/TextFile1.bat
@@ -1,3 +1,3 @@
 SET GALLIO_RUNTIME_PATH=C:\Users\PC\source\repos\mbunit-v3\src\Gallio\Gallio\bin
-"C:\Users\PC\source\repos\mbunit-v3\src\Extensions\Echo\Gallio.Echo\bin\Gallio.Echo.exe" "C:\TeamCity\buildAgent\work\f59c495a837c09b\TestProgram.Test\bin\x64\Debug\TestProgram.Test.dll" /r:AutoCAD /rp:AcadAttachToExisting=false /rp:AcadExePath="C:\Program Files\Autodesk\AutoCAD 2017\acad.exe"
+"C:\Users\PC\source\repos\mbunit-v3\src\Extensions\Echo\Gallio.Echo\bin\Gallio.Echo.exe" "C:\TeamCity\buildAgent\work\f59c495a837c09b\TestProgram.Test\bin\x64\Debug\TestProgram.Test.dll" /r:AutoCAD /rp:AcadAttachToExisting=false /rp:AcadExePath="C:\Program Files\Autodesk\AutoCAD 2018\acad.exe"
 pause
