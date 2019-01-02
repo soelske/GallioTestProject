aceda707de398932369a554b5aeaab26beb5106d
Bart Suelze <bartsuelze@hotmail.com>
2019-01-02 19:41:39 +0100

    test

--- TestProgram.Test/TextFile1.bat
+++ TestProgram.Test/TextFile1.bat
@@ -1,5 +1,3 @@
-echo %~dp0TestProgram.Test.dll
-pause
 SET GALLIO_RUNTIME_PATH=C:\Users\PC\source\repos\mbunit-v3\src\Gallio\Gallio\bin
 "C:\Users\PC\source\repos\mbunit-v3\src\Extensions\Echo\Gallio.Echo\bin\Gallio.Echo.exe" %~dp0TestProgram.Test.dll /r:AutoCAD /rp:AcadExePath="C:\Program Files\Autodesk\AutoCAD 2018\acad.exe"
 pause
